1c4bc6c504a8362e74b7c58eb1dd47e984665d4c
Gabriel <ggardiles@gmail.com>
2017-05-08 18:50:57 +0200

    ej4

--- GPO2/lab4.cpp
+++ GPO2/lab4.cpp
@@ -9,7 +9,8 @@ ATG, 2015
 char* WINDOW_TITLE="MI PRIMER OBJETO 3D (GpO)";
 int CurrentWidth = 600,   CurrentHeight = 450,  WindowHandle = 0;  // Tama�o ventana, handle a ventana
 unsigned FrameCount = 0;
-
+float azimuth = 0.6, elevacion = 0.6, D=3.0f;
+int mx0=0,my0=0;
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -79,6 +80,7 @@ objeto crear_cubo(void)
 						  1, 6, 2,
 						  2, 6, 7,
 						  2, 7, 3, };
+						  //3, 7, 2, };
 
 	obj.tipo_indice = GL_UNSIGNED_BYTE;
 	obj.Nv = 8;
@@ -137,11 +139,9 @@ void dibujar_indexado(objeto obj)
 void init_scene()  
 {
   obj1=crear_cubo();  // Datos del objeto, mandar a GPU
-  obj2=crear_cubo();
   prog=LinkShaders(vertex_prog,fragment_prog); // Compile shaders, crear programa a usar, Mandar a GPU
   glUseProgram(prog);    // Indicamos que programa vamos a usar 
 
-  glEnable(GL_DEPTH_TEST);
 }
 
 
@@ -155,9 +155,6 @@ vec3 up= vec3(0,0,1);
 // Actualizar escena: cambiar posici�n objetos, nuevos objetros, posici�n c�mara, luces, etc.
 void render_scene()
 {
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 	FrameCount++;
 	float tt = glutGet(GLUT_ELAPSED_TIME)/1000.0f;  // Tiempo en segundos
 
@@ -165,30 +162,16 @@ void render_scene()
 	glClear(GL_COLOR_BUFFER_BIT);                   // Aplica color asignado 
 
 	///////// Aqui vendr�a nuestr c�digo para actualizar escena  /////////
+	pos_obs = vec3(D*cos(azimuth)*cos(elevacion), D*sin(azimuth)*cos(elevacion), D*sin(elevacion));
 
 	mat4 P = perspective(40.0f, 4.0f / 3.0f, 0.1f, 20.0f);  //40� Y-FOV,  4:3 ,  Znear=0.1, Zfar=20
 	mat4 V = lookAt(pos_obs, target, up );  // Pos camara, Lookat, head up
 	mat4 M,T,R,S;
 
-	R = glm::rotate(30.0f*tt,vec3(0,0,1));
-	M = mat4(1.0f) * R;	
+	M = mat4(1.0f);
 
 	transfer_mat4("MVP",P*V*M);
     dibujar_indexado(obj1);
-
-	// trayectoria de r=1.5 y 1 vuelta cada 6 segundos
-	T = translate((float)(1.5 * sin((2*3.14*tt)/6.0)),
- 				  (float)(1.5 * cos((2*3.14*tt)/6.0)),
-				   0.0f);
-	// Escalado
-	S = glm::scale(0.8f, 0.4f ,0.2f);
-
-	// Rotacion 60º/segundos en el vector (1,0,1)
-	R = glm::rotate(60.0f*tt,vec3(1,0,1));
-	M = T * S * R;	
-
-	transfer_mat4("MVP",P*V*M);
-    dibujar_indexado(obj2);
 
 	////////////////////////////////////////////////////////
 
@@ -202,8 +185,9 @@ void render_scene()
 
 void keyboard(unsigned char, int, int);
 void key_special(int, int, int);
-void mouse(int,int, int, int);
+void mouse_click(int,int, int, int);
 void mouse_mov(int,int);
+void mouseWheel(int, int, int, int);
 
 
 void eventos_teclado_mouse()
@@ -211,8 +195,10 @@ void eventos_teclado_mouse()
 	glutKeyboardFunc(keyboard);      // Caso de pulsar alguna tecla
 //	glutSpecialFunc(key_special);  // Teclas de funci�n, cursores, etc
 
-//	glutMouseFunc();            // Eventos del rat�n
-//	glutPassiveMotionFunc();   // Mov del ra�n
+	glutMouseFunc(mouse_click);            // Eventos del rat�n
+	//glutPassiveMotionFunc(mouse_mov);   // Mov del ra�n
+	glutMotionFunc(mouse_mov);
+	glutMouseWheelFunc(mouseWheel);
 }
 
 
@@ -249,10 +235,36 @@ void key_special(int key, int x, int y)
 
 void mouse_click(int but,int state, int x, int y)
 {
+	mx0 = x;
+	my0 = y;
+
 }
 
 void mouse_mov(int x, int y)
 {
+	float dx = (float) (x - mx0);
+	float dy = (float) (y - my0);
+	printf("********************++dx=%f, dy=%f\n", dx,dy);
+	azimuth += 0.005f * dx;
+	elevacion += 0.005f * dy;
+	mx0 = x;
+	my0 = y;
+
+}
+void mouseWheel(int button, int dir, int x, int y)
+{
+    if (dir > 0)
+    {
+		// Zoom in
+        D += 0.005;
+    }
+    else
+    {
+		// Zoom out
+        D -= 0.005;
+    }
+
+    return;
 }
 
 
@@ -297,8 +309,8 @@ void Init_Opengl(void)
 void Init_Window(int argc, char* argv[])
 {
     glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE|GLUT_DEPTH);
-	//glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGBA);
+	
+	glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGBA);
     glutInitContextVersion(3,3);
     glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
     glutInitContextProfile(GLUT_CORE_PROFILE);
